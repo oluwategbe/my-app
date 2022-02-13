5345844798bf89218265d6b50f9777c16af37df1
Oluwategbe Pelumi <pelumitegbe@gmail.com>
2022-02-13 18:40:28 +0100

    Routes plus toggler plus links and stuf sha

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Menu from './MenuComponents';
+import Home from './HomeComponent';
 import { DISHES } from '../shared/dishes';
 import DishDetail from './DishdetailComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
 
@@ -21,9 +23,21 @@ class Main extends Component {
 
 
   render() {
+     
+    const HomePage = () => {
+      return(
+        <Home />
+      ); 
+    } 
+
     return (
       <div>
         <Header />
+        <Switch>
+            <Route path='/home' component={HomePage} />
+            <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+            <Redirect to="/home" />
+        </Switch>
         <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)}/>
         <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
         <Footer />
